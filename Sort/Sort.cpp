7ac101ecf49f98da6ee966fc20006c972c0059c7
Justin-Y-Park <y5ung@daum.net>
2017-02-15 00:00:36 +0900

    New Project for Sort Algorithms

---
+++ Sort/Sort.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define MAX_LENGTH	10
+
+void QuickSort(int data[]);
+void MergeSort(int data[]);
+void SelectionSort(int data[]);
+void BubbleSort(int data[]);
+
+int main()
+{
+	int data[MAX_LENGTH] = {2, 6, 5, 8, 4, 9, 1, 7, 10, 3};
+	
+
+	printf("Hello World\n");
+	return 0;
+}
+
